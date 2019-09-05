bf4a5d3c1327ed9770473dd0daf8e96b29ddfd81
Abhi Nayar <abhishek.nayar@yale.edu>
2019-09-04 21:46:29 -0700

    First Commit

--- README.md
+++ README.md
@@ -1,27 +1,26 @@
-# The tonebase Digest Creator - 2019
+# New Lesson Email Creator
 
 ### What is it?
 
-A quick way to create the Piano digest for weekly sending to the Piano subscribers. Eventually we will want to move the Guitar Digest to this format too as it is a little more extensible! This is a way easier and more extensible version than the app we used before and can easily be used to go between instruments.
+An INSANELY easy way to create new lesson emails. Simply clone this repo., edit the script file to include the slug of the lesson and wah-la, instamagic, you got the new lesson email. Thank me later ;)
 
 ### How to use?
 
 Follow the following directions:
 
 ```
-1. Clone this repo. => git clone https://github.com/tonebase/tonebase-Digest-Creator
-2. Make edits to GuitarData.json or PianoDigest.json as needed
-3. Navigate to the folder containing the index.html file in Terminal (lot of cd'ing, etc.)
-4. Start a local server with the following script: start8000
-5. Navigate to http://localhost:8000/guitar.html or http://localhost:8000/piano.html (respectively)
-6. You will see a preview of the newsletter/digest
-7. Wait 4-5 seconds - a new tab will open with the HTML data
-8. Make edits to the DigestData.json and keep refreshing the page to see the preview + get HTML
+1. Clone this repo. => git clone https://github.com/tonebase/tonebase-new-lesson-email-creator
+2. Make edits to the lessonScript.js file, particularly looking at: 'var lessonSlug =' and 'var showHTML ='
+3. Navigate to the folder you cloned down via Terminal and hit start8000 or python -m SimpleHTTPServer 8000
+4. Open your browser to http://localhost:8000
+5. Make sure everything looks good, then switch 'var showHTML = true' from any other value
+6. Hard refresh the preview page - you will get the generated HTML
+7. Test and send as needed
 ```
 
 This will do everything for you in terms of preview + generating HTML. Scheduling, batching and sending is another animal and we have other guides (or will have other guides) for that!
 
-### How to add to this repo ###
+### How to add to this repo
 
 When you make changes on your local machine to this repo, always push an update back so you have the most up to date version. In order to push to this repo. please enter the following commands:
 
@@ -31,7 +30,7 @@ When you make changes on your local machine to this repo, always push an update 
 3. git push origin master
 ```
 
-This will push code to the 'master' branch of github. 
+This will push code to the 'master' branch of Github.
 
 If you see a failure please talk to Abhi (abhi@tonebase.co) and he will help you. Most often the failure is caused by you not having the same data as the repo. You can fix this by doing the following:
 
@@ -44,7 +43,3 @@ If that causes more errors, reach out to Abhi immediately! Thanks!
 ### Questions, comments concerns?
 
 Reach out at abhi@tonebase.co
-
-### Credits
-
-Heavily based off of Chris Kellers script here, all credits to him: https://gist.github.com/chrislkeller/3230081
---
+++ lessonScript.js
@@ -0,0 +1,177 @@
+var jqueryNoConflict = jQuery;
+
+// === EDIT HERE ===
+const lessonSlug = "interview-with-marcin-dylla-miami-2019";
+const showHTML = true;
+
+// === SETUP HANDLEBARS TEMPLATES ==
+var templates = [
+  {
+    templateName: "preheaderTemplate.handlebars",
+    templateId: "#preheader-insert"
+  },
+  {
+    templateName: "logoTemplate.handlebars",
+    templateId: "#logo-insert"
+  },
+  {
+    templateName: "mainCTAImageTemplate.handlebars",
+    templateId: "#cta-image-insert"
+  },
+  {
+    templateName: "mainCTATemplate.handlebars",
+    templateId: "#main-cta-insert"
+  },
+  {
+    templateName: "metadataTemplate.handlebars",
+    templateId: "#metadata-insert"
+  },
+  {
+    templateName: "artistTemplate.handlebars",
+    templateId: "#artist-insert"
+  },
+  {
+    templateName: "artistCTATemplate.handlebars",
+    templateId: "#artist-cta-insert"
+  },
+  {
+    templateName: "stripScriptTemplate.handlebars",
+    templateId: "#strip-script"
+  }
+];
+
+// === SUB HELPERS ===
+
+function retriveData() {
+  const dataURL = `https://tonebase-api-production.herokuapp.com/v2/lessons/preview?slug=interview-with-marcin-dylla-miami-2019`;
+
+  jqueryNoConflict.getJSON(dataURL, function(data) {
+    console.log("JSON =>", data);
+
+    // Add show html property
+    data.showHTML = showHTML;
+
+    // Limit the description length
+    data.description = data.description.substring(0, 280);
+
+    // Create the wrapped data object for handlebars
+    var finalData = {
+      data: data
+    };
+
+    // Render handlebars templates
+    templates.forEach(function(templateData) {
+      var templateName = templateData.templateName;
+      var templateId = templateData.templateId;
+
+      if (templateName && templateId) {
+        renderHandlebarsTemplate(templateName, templateId, finalData);
+      }
+    });
+
+    // Fill in dyanmic info.
+    setTimeout(function() {
+      handleDynamicFields(data);
+    }, 3000);
+  });
+}
+
+function renderHandlebarsTemplate(withTemplate, inElement, withData) {
+  getTemplateAjax(withTemplate, function(template) {
+    jqueryNoConflict(inElement).html(template(withData));
+  });
+}
+
+function getTemplateAjax(path, callback) {
+  var source, template;
+  jqueryNoConflict.ajax({
+    url: `/templates/${path}`,
+    success: function(data) {
+      source = data;
+      template = Handlebars.compile(source);
+      if (callback) callback(template);
+    }
+  });
+}
+
+// === DYNAMIC ===
+
+function handleDynamicFields(data) {
+  const lessonType = jqueryNoConflict("#type_insert_dyna");
+  const lessonDifficulty = jqueryNoConflict("#difficulty_insert_dyna");
+  const metadataWrapper = jqueryNoConflict("#metadata_color_dyna");
+  const artistOneLiner = jqueryNoConflict("#artist_one_liner_dyna");
+
+  handleLessonType(lessonType, parseInt(data.lesson_type, 10));
+  handleLessonDifficulty(lessonDifficulty, parseInt(data.difficulty, 10));
+  handleMetadataColor(metadataWrapper, parseInt(data.lesson_type, 10));
+  handleArtistOneLiner(artistOneLiner, data.artist_slug);
+}
+
+function handleLessonType(DOMObj, lessonType) {
+  switch (lessonType) {
+    case 0:
+      jqueryNoConflict(DOMObj).text("Technique");
+      break;
+    case 1:
+      jqueryNoConflict(DOMObj).text("Repertoire");
+      break;
+    case 2:
+      jqueryNoConflict(DOMObj).text("Interview");
+      break;
+    default:
+      jqueryNoConflict(DOMObj).text("Lesson");
+  }
+}
+
+function handleLessonDifficulty(DOMObj, lessonDifficulty) {
+  switch (lessonDifficulty) {
+    case 0:
+      jqueryNoConflict(DOMObj).text("All Levels");
+      break;
+    case 1:
+      jqueryNoConflict(DOMObj).text("Intermediate");
+      break;
+    case 2:
+      jqueryNoConflict(DOMObj).text("Advanced");
+      break;
+    default:
+      jqueryNoConflict(DOMObj).text("All Levels");
+  }
+}
+
+function handleMetadataColor(DOMObj, lessonType) {
+  switch (lessonType) {
+    case 0:
+      jqueryNoConflict(DOMObj).attr("style", "color:#faa000;");
+      break;
+    case 1:
+      jqueryNoConflict(DOMObj).attr("style", "color:#e14f3d;");
+      break;
+    case 2:
+      jqueryNoConflict(DOMObj).attr("style", "color:#ffc82c;");
+      break;
+    default:
+      jqueryNoConflict(DOMObj).attr("style", "color:#e14f3d;");
+  }
+}
+
+function handleArtistOneLiner(DOMObj, artistSlug) {
+  console.log("HANDLE ARTIST ONE LINER");
+  const dataURL = `http://tonebase-api-production.herokuapp.com/v2/artists/narrow?slug=${artistSlug}&property=one_liner`;
+  console.log(dataURL);
+
+  jqueryNoConflict.get(dataURL, function(data) {
+    console.log("Artist One Liner =>", data);
+
+    if (data) {
+      jqueryNoConflict(DOMObj).text(data);
+    }
+  });
+}
+
+// === MAIN ===
+
+jqueryNoConflict(document).ready(function() {
+  retriveData();
+});
